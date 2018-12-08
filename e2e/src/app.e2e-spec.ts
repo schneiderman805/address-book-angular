d08b4a2098319599ed3dba0a0d28651e7978ea4b
William Schneiderman <william.m.schneiderman@gmail.com>
2018-12-08 21:51:45 +0100

    adds new it blocks to app.e2e-spec.ts

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -5,13 +5,31 @@ describe('workspace-project App', () => {
 
   beforeEach(() => {
     page = new AppPage();
-    page.navigateTo();
+    
   });
 
   
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to address-book!');
+  it('should display create contact header', () => {
+    expect(page.getParagraphText()).toEqual('Create contact');
   });
+
+  it('should add a new contact', () => {
+    let count = page.cardCount();
+    expect(count).toBe(0);
+
+    page.getField('name').sendKeys('John Doe');
+    page.getField('email').sendKeys('john@craftacademy.se');
+    page.getField('company').sendKeys('Craft Academy');
+    page.getField('role').sendKeys('Tester');
+    page.getField('twitter').sendKeys('@tester');
+    page.getField('location').sendKeys('Gothenburg');
+    page.getField('notes').sendKeys('Some tester guy');
+
+    page.getSubmitButton().click();
+
+    count = page.cardCount();
+    expect(count).toBe(1);
+  });
+
 });
